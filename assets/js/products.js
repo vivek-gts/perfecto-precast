ee9730d4cf1f3660ba3af7f8adb641b3a7104457
Vivek Yadav <vivek.grabthesite@gmail.com>
2024-09-05 18:41:18 +0530

    product page

---
+++ assets/js/products.js
@@ -0,0 +1,337 @@
+const productsList = [
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443010886/LI/XO/JO/7407915/t6-300mm-rcc-precast-u-drain-heavy-500x500.jpg",
+    name: "T6 300x300 mm RCC Precast U Drain Heavy",
+    price: "₹ 5,500/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443018065/RA/JP/ZF/7407915/t-6-750mm-rcc-precast-u-shape-drain-500x500.jpg",
+    name: "T6 750X750 mm RCC Precast U Shape Drain",
+    price: "₹ 5,550/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443018454/EK/IL/CJ/7407915/t-6-600mm-rcc-precast-u-shape-drain-500x500.jpg",
+    name: "T6 600x600 mm RCC Precast U Shape Drain",
+    price: "₹ 5,550/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443018741/QL/RV/WK/7407915/t450mm-rcc-precast-u-shape-drain-500x500.jpg",
+    name: "T6 450x450 mm RCC Precast U Shape Drain",
+    price: "₹ 5,550/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443011192/MA/GC/QT/7407915/t6-1200mm-rcc-precast-u-drain-heavy-500x500.jpg",
+    name: "T25 1200x1200 mm RCC Precast U Drain Heavy",
+    price: "₹ 7,500/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443013029/EK/VH/DZ/7407915/t6-600mm-rcc-precast-u-drain-heavy-500x500.jpg",
+    name: "T25 600X600 mm RCC Precast U Drain Heavy",
+    price: "₹ 4,550/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443012174/SK/KX/IT/7407915/t6-750mm-rcc-precast-u-drain-heavy-500x500.jpg",
+    name: "T25 750x750 mm RCC Precast U Drain Heavy",
+    price: "₹ 6,250/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443012662/MR/BU/LI/7407915/t6-450mm-rcc-precast-u-drain-heavy-500x500.jpg",
+    name: "T25 450 x450 mm RCC Precast U Drain Heavy",
+    price: "₹ 4,500/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443017347/TR/QJ/ET/7407915/t-6-300mm-rcc-precast-u-shape-drain-500x500.jpg",
+    name: "T25 300 x 300 RCC Precast U Shape Drain",
+    price: "₹ 2,500/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443017710/IE/IM/HM/7407915/t-6-900mm-rcc-precast-u-shape-drain-500x500.jpg",
+    name: "T6 900x 900 Mm RCC Precast U Shape Drain",
+    price: "₹ 5,500/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443019397/ZH/YH/AK/7407915/rcc-precast-ft-flume-drain-500x500.jpg",
+    name: "RCC Precast FT Flume Drain",
+    price: "₹ 6,500/ Meter",
+  },
+  {
+    category: "Precast U Drain",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443011797/NA/PR/XB/7407915/t6-900mm-rcc-precast-u-drain-heavy-500x500.jpg",
+    name: "T25 900x900 mm RCC Precast U Drain Heavy",
+    price: "₹ 6,500/ Meter",
+  },
+  {
+    category: "Box Culvert",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443014780/ED/NV/MB/7407915/1000mm-precast-rcc-box-culvert-500x500.jpg",
+    name: "1000 mm Precast RCC Box Culvert",
+    price: "₹ 6,500/ Meter",
+  },
+  {
+    category: "Box Culvert",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443015205/BU/XH/AY/7407915/1200mm-precast-rcc-box-culvert-500x500.jpg",
+    name: "1700X1200 mm Precast RCC Box Culvert",
+    price: "₹ 24,450/ Meter",
+  },
+  {
+    category: "Box Culvert",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443015382/PS/LM/ZL/7407915/1400mm-precast-rcc-box-culvert-500x500.jpg",
+    name: "1400 mm Precast RCC Box Culvert",
+    price: "₹ 30,500/ Meter",
+  },
+  {
+    category: "Box Culvert",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443015591/ZL/GD/GG/7407915/1500mm-precast-rcc-box-culvert-500x500.jpg",
+    name: "1500 X1500 mm Precast RCC Box Culvert",
+    price: "₹ 30,500/ Meter",
+  },
+  {
+    category: "Box Culvert",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443015819/NY/IC/IB/7407915/1800mm-precast-rcc-box-culvert-500x500.jpg",
+    name: "1800X1800 mm Precast RCC Box Culvert",
+    price: "₹ 35,000/ Meter",
+  },
+  {
+    category: "Box Culvert",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443016321/XK/UR/QP/7407915/1300mm-precast-rcc-box-culvert-500x500.jpg",
+    name: "Precast RCC Box Culvert",
+    price: "₹ 28,500/ Meter",
+  },
+  {
+    category: "Box Culvert",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/443016055/UH/KP/SS/7407915/2000mm-precast-rcc-box-culvert-500x500.jpg",
+    name: "2000X2000 mm Precast RCC Box Culvert",
+    price: "₹ 39,500/ Meter",
+  },
+  {
+    category: "Precast Bollards",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2024/8/440878386/ZH/MR/YT/7407915/whatsapp-image-2024-08-06-at-1-00-03-pm-500x500.jpeg",
+    name: "Precast Concrete Bollard",
+    price: "₹ 2,500/ Piece",
+  },
+  {
+    category: "Paver Block",
+    imageUrl:
+      "https://5.imimg.com/data5/XI/EL/PU/SELLER-32985232/5-500x500.jpg",
+    name: "High Shape Pavers",
+    price: "Rs 34/ Sqft",
+  },
+  {
+    category: "Paver Block",
+    imageUrl:
+      "https://5.imimg.com/data5/TS/TO/ID/SELLER-32985232/6-500x500.jpg",
+    name: "Rectangular Pavers Blocks",
+    price: "Rs 37/ Sqft",
+  },
+  {
+    category: "Paver Block",
+    imageUrl:
+      "https://5.imimg.com/data5/EJ/CZ/AN/SELLER-32985232/9-500x500.jpg",
+    name: "Paver Block (Eleven by eight)",
+    price: "Rs 34/ Sqft",
+  },
+  {
+    category: "Paver Block",
+    imageUrl:
+      "https://5.imimg.com/data5/VX/HG/JP/SELLER-32985232/7-500x500.jpg",
+    name: "Square Paver Blocks",
+    price: "Rs 34/ Sqft",
+  },
+  {
+    category: "Paver Block",
+    imageUrl:
+      "https://5.imimg.com/data5/PH/EL/CU/SELLER-32985232/8-500x500.jpg",
+    name: "Zig Zag Paver Blocks",
+    price: "Rs 35/ Sqft",
+  },
+  {
+    category: "Paver Block",
+    imageUrl:
+      "https://5.imimg.com/data5/OG/CG/VG/SELLER-32985232/last-500x500.jpg",
+    name: "Hexagon Paver Block",
+    price: "Rs 37/ Sqft",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/ANDROID/Default/2023/12/366752155/NS/AG/LB/32985232/product-jpeg-500x500.jpg",
+    name: "Dotted Kerb Stone",
+    price: "Rs 165/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/YY/VY/CJ/SELLER-32985232/11-500x500.jpg",
+    name: "Ramp Kerb Stone",
+    price: "Rs 150/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/QL/FE/XP/SELLER-32985232/hydraulic-kerb-stone-500x500.jpg",
+    name: "Hydraulic kerb stone",
+    price: "Rs 95/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/YJ/HC/CR/SELLER-32985232/10-500x500.jpg",
+    name: "Landscaping Kerb Stone",
+    price: "Rs 95/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/CH/SH/CP/SELLER-32985232/8-500x500.jpg",
+    name: "Plus Kerb Stone",
+    price: "Rs 145/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/XO/FV/YN/SELLER-32985232/new-500x500.jpg",
+    name: "Bullnose Kerb Stone",
+    price: "Rs 150/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/LT/AN/VG/SELLER-32985232/7-500x500.jpg",
+    name: "Vent Kerb Stone",
+    price: "Rs 150/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/YG/GG/JM/SELLER-32985232/6-500x500.jpg",
+    name: "Off Round Kerb Stone",
+    price: "Rs 145/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/UI/ZT/UX/SELLER-32985232/5-500x500.jpg",
+    name: "Kerbs Stone Manufacturer",
+    price: "Rs 145/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/JF/XM/DH/SELLER-32985232/8d6c6916-40ad-4a29-ba18-9de794693ebc-500x500.jpg",
+    name: "Kerbs Stone Suppliers",
+    price: "Rs 195/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/MD/NU/ZC/SELLER-32985232/4f6d8324-af40-493a-be1a-4f3387b5671e-500x500.jpg",
+    name: "Rcc Kerb Stone",
+    price: "Rs 215/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/ANDROID/Default/2023/12/366740609/BU/KV/NX/32985232/product-jpeg-500x500.jpg",
+    name: "Kerb Stone Paver Block",
+    price: "Rs 150/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/ANDROID/Default/2023/12/366729306/ZP/SA/TN/32985232/product-jpeg-500x500.jpg",
+    name: "Precast Concrete Kerbstone",
+    price: "Rs 150/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/AT/ZA/TU/SELLER-32985232/kerb-stone-500x500.jpeg",
+    name: "Hydraulic Concrete Kerbstone",
+    price: "Rs 95/ Piece",
+  },
+  {
+    category: "Kerb Stone",
+    imageUrl:
+      "https://5.imimg.com/data5/NO/FG/KW/SELLER-32985232/kerb-stone-500x500.jpg",
+    name: "Hydraulic kerb stone",
+    price: "Rs 95/ Piece",
+  },
+];
+
+var culvertWrapper = document.querySelector("#culvert .row");
+var drainWrappper = document.querySelector("#drain .row");
+var kerbWrapper = document.querySelector("#kerb .row");
+var paverWrapper = document.querySelector("#paver .row");
+
+productsList.forEach((el) => {
+  const productNode = document.createElement("div");
+  productNode.classList.add("col-lg-3", "col-md-4", "col-sm-6");
+  productNode.innerHTML = `<figure class="product-wrapper white-bg mb-45">
+                  <div class="product-thumb">
+                    <a href="product-details.html"
+                      ><img
+                        src="${el.imageUrl}"
+                        class="figure-img w-100 product-image"
+                        alt="product_img"
+                    /></a>
+                  </div>
+                  <figcaption class="figure-caption">
+                    <div class="product-tag">${el.category}</div>
+                    <h4 class="product-title">
+                      <a href="product-details.html"
+                        >${el.name}</a
+                      >
+                    </h4>
+                    <div class="price">
+                      <span class="old-price">${el.price}</span>
+                    </div>
+                  </figcaption>
+                </figure>`;
+  switch (el.category) {
+    case "Box Culvert":
+      culvertWrapper.appendChild(productNode);
+      break;
+    case "Precast U Drain":
+      drainWrappper.appendChild(productNode);
+      break;
+    case "Paver Block":
+      paverWrapper.appendChild(productNode);
+      break;
+    case "Kerb Stone":
+      kerbWrapper.appendChild(productNode);
+      break;
+
+    default:
+      break;
+  }
+});
