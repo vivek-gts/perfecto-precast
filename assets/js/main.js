3bf5af79a4c1b55edf4bc6fc18dd44bf27253c27
Vivek Yadav <vivek.grabthesite@gmail.com>
2024-09-06 14:15:24 +0530

    about page and home page changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,9 +275,14 @@
     },
 
     // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
 
     breakpoints: {
